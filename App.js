1e04ad84e69a8f94f272b280a711b9f7b7a8681e
RameshBandi123 <bandiramesh8802@gmail.com>
2025-01-27 22:57:08 +0530

    2nd commit

--- App.js
+++ App.js
@@ -1,3 +1,11 @@
-const heading=React.createElement('h1',{},"hello world");
+import React from 'react';
+import ReactDOM from 'react-dom/client'; 
+
+const parent=React.createElement('div',{id:"parent"},[
+    React.createElement('div',{id:"child"},[
+        React.createElement("h1",{},"I am an h1 tag")
+    ])
+])
+
         const root=ReactDOM.createRoot(document.getElementById("root"));
-        root.render(heading)
+        root.render(parent)
---
+++ README.md
@@ -0,0 +1,16 @@
+// Dev build
+Local server
+HMR =HOT MODULE REPLACEMENT
+-File Matching Algorithm-written in C++
+-Caching- Faster builds
+-image optimization
+-Minification
+-Bundling
+-Compressing
+-consistent Hashing
+-code splitting
+-Differential bundling
+-Error Handling
+-DIagnostic
+-HTTPS
+-Tree Shaking- remove unused code
